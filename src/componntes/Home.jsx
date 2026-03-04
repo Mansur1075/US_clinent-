c260833e1eede0ebfb461c55dd6e802bb56aa38d
Mansur1075 <hmmansur050@gmail.com>
2026-03-04 10:19:32 -0800

    mansur

--- src/componntes/Home.jsx
+++ src/componntes/Home.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import Container from './Container'
 import mansur from "../assets/mansur.png"
+import { FaFacebook } from 'react-icons/fa'
+import { ImFacebook2 } from 'react-icons/im'
+import { AiFillInstagram, AiOutlineYoutube } from 'react-icons/ai'
 
 const Home = () => {
   return (
-    <div className='bg-[black]'>
+    <div className='bg-[#000000]'>
       <Container>
-        <div className=" flex py-4 text-[#ffff] justify-between">
-          <img src={mansur} alt="" />
-          <h3 className='text-amber-600'>mansur</h3>
-          <h3>mansur</h3>
+        <div className=" flex py-4 text-[#ffff] justify-between items-center">
+         <div className="">
+           <img src={mansur} alt="" />
+         </div>
+          <div className="w-7/12 orbitron">
+            <ul className=' flex  gap-20'>
+              <li>home</li>
+              <li>about</li>
+              <li>portfolio</li>
+              <li>testimonials</li>
+              <li>contact</li>
+            </ul>
+          </div>
+          <div className=" items-center flex gap-5 ">
+            <ImFacebook2/>
+            <AiFillInstagram className='text-2xl'/>
+            <AiOutlineYoutube className=' text-2xl' />
+          </div>
         </div>
       </Container>
     </div>
