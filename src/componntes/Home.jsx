32305ba54590cc31d9c4aeb0429fcb13c023d89c
Mansur1075 <hmmansur050@gmail.com>
2026-03-04 09:26:12 -0800

    mansur

---
+++ src/componntes/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Container from './Container'
+import mansur from "../assets/mansur.png"
+
+const Home = () => {
+  return (
+    <div className='bg-[black]'>
+      <Container>
+        <div className=" flex py-4 text-[#ffff] justify-between">
+          <img src={mansur} alt="" />
+          <h3>mansur</h3>
+          <h3>mansur</h3>
+        </div>
+      </Container>
+    </div>
+  )
+}
+
+export default Home
